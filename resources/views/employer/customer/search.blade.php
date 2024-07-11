501e313d9c4a98d0a106c21c626822b30d1b468d
ReonaOhmine <r.ohmine@freddy.co.jp>
2024-07-12 00:08:56 +0900

    一つ前にに戻す

--- resources/views/employer/customer/search.blade.php
+++ resources/views/employer/customer/search.blade.php
@@ -167,12 +167,7 @@
         window.openModal = function (id) {
             console.log("Opening modal for candidate with ID:", id);
             fetch(`/api/get-candidate/${id}`)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Network response was not ok');
-                    }
-                    return response.json();
-                })
+                .then(response => response.json())
                 .then(data => {
                     const age = calculateAge(data.birthday);
                     document.getElementById('modal-title').textContent = '詳細情報';
