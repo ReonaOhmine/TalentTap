f3f9deeaba083d04d091fa3b1e94ff7867504d29
ReonaOhmine <r.ohmine@freddy.co.jp>
2024-07-12 07:55:21 +0900

    appjs変更　VITE_PUSHER_APP_KEYの確認用

--- resources/js/app.js
+++ resources/js/app.js
@@ -11,11 +11,18 @@ import Pusher from 'pusher-js';
 
 window.Pusher = Pusher;
 
+console.log('Pusher Key:', import.meta.env.VITE_PUSHER_APP_KEY);
+console.log('Pusher Cluster:', import.meta.env.VITE_PUSHER_APP_CLUSTER);
+
 window.Echo = new Echo({
     broadcaster: 'pusher',
     key: import.meta.env.VITE_PUSHER_APP_KEY,
     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
     forceTLS: true
+});
+
+window.Echo.connector.pusher.connection.bind('error', function(err) {
+    console.error('WebSocket error:', err);
 });
 
 window.Echo.channel('example-channel')
