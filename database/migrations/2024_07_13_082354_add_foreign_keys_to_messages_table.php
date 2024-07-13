faabb6c850090b1631bcf686e42ab0de3154d6fa
ReonaOhmine <r.ohmine@freddy.co.jp>
2024-07-13 17:27:01 +0900

    messageテーブルの外部キー制約を修正

---
+++ database/migrations/2024_07_13_082354_add_foreign_keys_to_messages_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->foreign('sender_id')->references('id')->on('agents')->onDelete('cascade');
+            $table->foreign('receiver_id')->references('id')->on('employer_users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('messages', function (Blueprint $table) {
+            $table->dropForeign(['sender_id']);
+            $table->dropForeign(['receiver_id']);
+        });
+    }
+};
